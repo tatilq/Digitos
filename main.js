654177cf215f89e6e2b22e6d85cdbce2b47193cf
Tatiana Limache Quispe <tati34che@gmail.com>
2017-01-03 02:30:37 -0500

    modificacion adicional

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 
-function cantidadDigitos(){
+function cantidadDigitos()
+{
     var numero = document.getElementById("numero").value;
     var salida = document.getElementById("salida");
     var c=0;
  
+ if (!/^([0-9])*$/.test(numero)){
+    salida.innerHTML = "--";
+  	document.getElementById("mensajes").innerHTML = '<div class="alert alert-danger">¡No es un Numero!</div>';
+	var numero = document.getElementById("numero").value=" ";
+  }
+ else{
     while(numero>0)
     {
-
       numero = parseInt(numero/10);
       c++;
     }
  
   salida.innerHTML = c;
-   
+  document.getElementById("mensajes").innerHTML = '<div >Digitos</div>';
+			
+	}
 }
